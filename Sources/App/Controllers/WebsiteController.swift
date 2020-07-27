8043f6e6da8cac5147a64852c16b12797f8e0ad5
Fitgerald Afful <fitzafful@gmail.com>
2020-07-27 21:28:00 +0000

    Add Employee using modal

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -5,18 +5,25 @@ struct WebsiteController: RouteCollection {
 
     func boot(router: Router) throws {
         router.get(use: indexHandler)
+        router.post(Employee.self, at: "index/add", use: createEmployeeHandler)
     }
 
     func indexHandler(_ req: Request) throws -> Future<View> {
-        return Employee.query(on: req).all().flatMap(to: View.self) { employees in // 2
+        return Employee.query(on: req).all().flatMap(to: View.self) { employees in
             let employeesData = employees.isEmpty ? nil : employees
             let context = IndexContext(title: "Employees List", employees: employeesData)
             return try req.view().render("index", context)
         }
     }
+
+    func createEmployeeHandler(_ req: Request, employee: Employee) throws -> Future<Response> {
+        return employee.save(on: req).map(to: Response.self) { _ in
+            return req.redirect(to: "index")
+        }
+    }
 }
 
 struct IndexContext: Encodable {
-  let title: String
-  let employees: [Employee]?
+    let title: String
+    let employees: [Employee]?
 }
